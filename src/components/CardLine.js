866eaccf1d770132900b652b853ae71715af39bb
nmcanall <nmcanall@citadel.edu>
2022-02-11 13:11:46 -0600

    code refactor

--- src/components/CardLine.js
+++ src/components/CardLine.js
@@ -1,35 +1,18 @@
 import Card from "./card/Card";
-import katie from "../styles/images/katie.png";
-import wedding from "../styles/images/wedding.png";
-import mountainBike from "../styles/images/mountain-bike.png";
+import data from "../data";
 
 function CardLine() {
+
+    var dataElements = data.map(item => {
+        return <Card
+            key={item.id}
+            item={item}
+        />
+    });
+
     return (
         <div className="card-line">
-            <Card
-                img={katie}
-                rating="5.0"
-                numRatings="6"
-                country="USA"
-                desciption="Life lessons with Katie Zaferes"
-                cost="136"
-            />
-            <Card
-                img={wedding}
-                rating="5.0"
-                numRatings="30"
-                country="USA"
-                desciption="Learn wedding photography"
-                cost="125"
-            />
-            <Card
-                img={mountainBike}
-                rating="4.8"
-                numRatings="2"
-                country="USA"
-                desciption="Group Mountain Biking"
-                cost="50"
-            />
+            {dataElements}
         </div>
     )
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,29 @@
 import star from "../../styles/images/star.png";
 
-function Card(props) {
+function Card({item}) {
+    console.log(item);
+    let badgeText;
+    if(item.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    }
+    else if (item.location === "Online") {
+        badgeText = item.location;
+    }
+
     return (
         <div className="card">
-            <img src={props.img} className="card-img" alt="activity preview"/>
+            {badgeText && <div className="badge">{badgeText}</div>}
+            <img src={`./images/${item.coverImg}`} className="card-img" alt="activity preview"/>
             <div className="details">
                 <img src={star} className="star" alt="Simple star icon"/>
-                <p className="rating">{props.rating}</p>
-                <p className="grey-text num-ratings">{props.numRatings}</p>
+                <p className="rating">{item.stats.rating}</p>
+                <p className="grey-text num-ratings">({item.stats.reviewCount})</p>
                 <p className="ellipse grey-text">•</p>
-                <p className="grey-text country">{props.country}</p>
+                <p className="grey-text country">{item.location}</p>
+                
             </div>
-            <p>{props.description}</p>
-            <p><span style={{fontWeight: "bold"}}>From ${props.cost}</span> / person</p>
+            <p>{item.title}</p>
+            <p><span style={{fontWeight: "bold"}}>From ${item.price}</span> / person</p>
         </div>
     )
 }
